24aae675a82c2f4ea33f6b7b8edc7cff3a0a1238
Vitor Wagner <vitormwagner@gmail.com>
2023-08-29 15:46:12 -0300

    Feat: :sparkles: Simple profile template with customized viewset

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.shortcuts import render
 from projects.models import Profile, Project
 from .serializers import ProfileSerializer, ProjectSerializer
 
@@ -6,6 +8,20 @@ from .serializers import ProfileSerializer, ProjectSerializer
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            instance = self.get_object()
+            serializer = self.get_serializer(instance)
+            return render(
+                request, "profile_detail.html", {"profile": serializer.data}
+            )
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
